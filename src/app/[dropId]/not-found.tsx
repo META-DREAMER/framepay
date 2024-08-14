cbdff1edec9561648db7d3f61815f1480eddffa6
META DREAMER <jutt@ualberta.ca>
2024-08-13 19:39:47 -0600

    fix lint error

--- src/app/[dropId]/not-found.tsx
+++ src/app/[dropId]/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
         </CardHeader>
         <CardContent>
           <p className="text-gray-600">
-            Oops! The page you're looking for doesn't exist or has been moved.
+            Oops! The page you&apos;re looking for doesn&apos;t exist or has been moved.
           </p>
         </CardContent>
       </Card>
